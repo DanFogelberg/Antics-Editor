7c3b67130422a712474b1e815edbf1920607d711
lovbackan <simon@lovbacka.se>
2023-05-29 14:03:35 +0200

    Button component added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 import { Editor } from "./components/Editor/Editor.tsx";
 import { Preview } from "./components/Preview/Preview.tsx";
+import { Button } from "./components/Button/Button.tsx";
 //Bugg som gör att vi inte kan ha .tsx filer i import. Ändra i import package.json filen för att lösa detta.
 
 const ContentWrapper = styled.div`
@@ -23,6 +24,11 @@ function App() {
         <Editor setText={updateText} />
         <Preview text={text} />
       </ContentWrapper>
+      <Button
+        handleClick={() => console.log("hej")}
+        className="hej"
+        text="Save"
+      />
     </div>
   );
 }
